8f0e1bce9848529223dd2f1091e0b0eca5b2f7d1
Aurora28 <472645313@qq.com>
2020-07-02 00:21:45 -0700

    navigate withRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
-import HomePage from './pages/homepage/homepage.component.jsx'
+import { Switch, Route } from 'react-router-dom';
+import HomePage from './pages/homepage/homepage.component.jsx';
+
+const HatsPage = (props) => (
+  <div>
+    <h1>HAT Page</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-     <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
